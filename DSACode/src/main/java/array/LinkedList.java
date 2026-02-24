eb72ff3482a2e13503ebfac42e4a8c4605daa407
Sumit Kumar Singh <34736482+amiablemusafir@users.noreply.github.com>
2026-02-24 22:28:30 +0530

    Linked list

---
+++ DSACode/src/main/java/array/LinkedList.java
@@ -0,0 +1,44 @@
+package array;
+
+public class LinkedList {
+    Node head;
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            next = null;
+        }
+    }
+
+    public void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.insertAtEnd(10);
+        list.insertAtEnd(20);
+        list.insertAtEnd(30);
+        list.printList(); // Output: 10 20 30
+    }
+}
