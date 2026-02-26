64e1502a4f90c25fff574e9c21b6f10d4fb6bbbe
Sumit Kumar Singh <34736482+amiablemusafir@users.noreply.github.com>
2026-02-26 22:26:55 +0530

    Binary tree

---
+++ DSACode/src/main/java/array/BinaryTree.java
@@ -0,0 +1,33 @@
+package array;
+
+class BinaryTree {
+    Node root;
+
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            left = right = null;
+        }
+    }
+
+    void inOrder(Node node) {
+        if (node == null) return;
+        inOrder(node.left);
+        System.out.print(node.data + " ");
+        inOrder(node.right);
+    }
+
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+
+        tree.inOrder(tree.root); // Output: 4 2 5 1 3
+    }
+}
--- DSACode/src/main/java/array/OddEvenRecursion.java
+++ DSACode/src/main/java/array/OddEvenRecursion.java
@@ -1,2 +1,15 @@
-package array;public class OddEvenRecursion {
+package array;
+
+public class OddEvenRecursion {
+    public boolean isEven(int n) {
+        if(n == 0) return true;
+        if(n == 1) return false;
+        return isEven(n-2);
+    }
+
+    public static void main(String[] args) {
+        OddEvenRecursion oe = new OddEvenRecursion();
+        System.out.println(oe.isEven(12));
+    }
+
 }
