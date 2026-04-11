f325954ab7188ccd1f7cc1808ca3ed774aba699b
Sumit Kumar Singh <34736482+amiablemusafir@users.noreply.github.com>
2026-04-11 07:47:11 +0530

    Count Character Frequency

---
+++ DSACode/src/main/java/Top20Questions/CharFrequency.java
@@ -0,0 +1,16 @@
+package Top20Questions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CharFrequency {
+    public static void main(String[] args) {
+        String str = "java";
+        Map<Character, Integer> map = new HashMap<>();
+
+        for(char ch: str.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        System.out.println(map);
+    }
+}
