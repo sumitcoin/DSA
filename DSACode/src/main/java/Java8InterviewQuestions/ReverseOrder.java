e087fd053d0160d69d3eb4216a797be660fa8da5
Sumit Kumar Singh <34736482+amiablemusafir@users.noreply.github.com>
2026-04-14 08:39:09 +0530

    How do you sort the given list of decimals in reverse order?

---
+++ DSACode/src/main/java/Java8InterviewQuestions/ReverseOrder.java
@@ -0,0 +1,13 @@
+package Java8InterviewQuestions;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class ReverseOrder {
+    public static void main(String[] args) {
+        List<Double> decimalList = Arrays.asList(12.45, 23.58, 17.13, 42.89, 33.78, 71.85, 56.98, 21.12);
+
+        decimalList.stream().sorted(Comparator.reverseOrder()).forEach(System.out::println);
+    }
+}
