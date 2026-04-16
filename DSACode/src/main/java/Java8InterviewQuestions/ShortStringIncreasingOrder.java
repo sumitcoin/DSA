6aaafc2a860d382dcba5db84d429e4bddabb1909
Sumit Kumar Singh <34736482+amiablemusafir@users.noreply.github.com>
2026-04-16 09:59:57 +0530

    Given a list of strings, sort them according to increasing order of their length?

---
+++ DSACode/src/main/java/Java8InterviewQuestions/ShortStringIncreasingOrder.java
@@ -0,0 +1,15 @@
+package Java8InterviewQuestions;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+// Given a list of strings, sort them according to increasing order of their length?
+public class ShortStringIncreasingOrder {
+    public static void main(String[] args) {
+        List<String> listOfStrings = Arrays.asList("Java", "Python", "C#", "HTML", "Kotlin", "C++", "COBOL", "C");
+
+        listOfStrings.stream().sorted(Comparator.comparing(String::length)).forEach(System.out::println);
+
+    }
+}
