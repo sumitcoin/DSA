8aafe35f59338704d33de22dd2287ded5f794a86
Sumit Kumar Singh <34736482+amiablemusafir@users.noreply.github.com>
2026-04-18 08:44:38 +0530

    Given a list of strings, find out those strings which start with a number?

---
+++ DSACode/src/main/java/Java8InterviewQuestions/StringWithNumber.java
@@ -0,0 +1,13 @@
+package Java8InterviewQuestions;
+
+import java.util.Arrays;
+import java.util.List;
+
+// Given a list of strings, find out those strings which start with a number?
+public class StringWithNumber {
+    public static void main(String[] args) {
+        List<String> listOfStrings = Arrays.asList("One", "2wo", "3hree", "Four", "5ive", "Six");
+
+        listOfStrings.stream().filter(str -> Character.isDigit(str.charAt(0))).forEach(System.out::println);
+    }
+}
