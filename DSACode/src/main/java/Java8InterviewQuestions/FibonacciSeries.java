237cb932e7fb120e89068c4b8b43e3ff9e51b879
Sumit Kumar Singh <34736482+amiablemusafir@users.noreply.github.com>
2026-04-21 20:00:06 +0530

    Fibonacci series

---
+++ DSACode/src/main/java/Java8InterviewQuestions/FibonacciSeries.java
@@ -0,0 +1,13 @@
+package Java8InterviewQuestions;
+
+import java.util.stream.Stream;
+
+// Fibonacci series
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        Stream.iterate(new int[] {0, 1}, f -> new int[] {f[1], f[0]+f[1]})
+                .limit(10)
+                .map(f -> f[0])
+                .forEach(i -> System.out.println(i+" "));
+    }
+}
