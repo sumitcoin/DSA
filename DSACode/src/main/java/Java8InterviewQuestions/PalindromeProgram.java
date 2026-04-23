7a062cc7b42f06b14ea96422f54c392c471a91e2
Sumit Kumar Singh <34736482+amiablemusafir@users.noreply.github.com>
2026-04-23 22:02:26 +0530

    First 10 odd numbers

---
+++ DSACode/src/main/java/Java8InterviewQuestions/PalindromeProgram.java
@@ -0,0 +1,19 @@
+package Java8InterviewQuestions;
+
+import java.util.stream.IntStream;
+
+// Palindrome program using Java 8 streams
+public class PalindromeProgram {
+
+    public static void main(String[] args) {
+        String str = "ROTATOR";
+        boolean isItPalindrome = IntStream.range(0, str.length()/2)
+                .noneMatch(i -> str.charAt(i) != str.charAt(str.length() - i -1));
+
+        if(isItPalindrome) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+    }
+}
