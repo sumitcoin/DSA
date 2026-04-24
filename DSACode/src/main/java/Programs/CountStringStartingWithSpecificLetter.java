f6aed8fc53977fa0d8760c9e5b9857946a46ec15
Sumit Kumar Singh <34736482+amiablemusafir@users.noreply.github.com>
2026-04-24 20:47:35 +0530

    Find word starting with a specific letter

---
+++ DSACode/src/main/java/Programs/CountStringStartingWithSpecificLetter.java
@@ -0,0 +1,16 @@
+package Programs;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+// Find word starting with a specific letter
+public class CountStringStartingWithSpecificLetter {
+    public static void main(String[] args) {
+        List<String> strList = Arrays.asList("Ajay","Shyam","Arjun","Ram","Arti","Sonu","Monu");
+
+        List<String> filterStringList = strList.stream().filter(a -> a.startsWith("A")).collect(Collectors.toList());
+
+        System.out.println(filterStringList);
+    }
+}
