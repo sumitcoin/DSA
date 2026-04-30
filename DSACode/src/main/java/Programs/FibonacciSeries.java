56bfed3b97aee44b5d454d3def7979229218c05f
Sumit Kumar Singh <34736482+amiablemusafir@users.noreply.github.com>
2026-04-30 22:18:40 +0530

    Fibonacci Series using java

--- DSACode/src/main/java/Programs/FibonacciSeries.java
+++ DSACode/src/main/java/Programs/FibonacciSeries.java
@@ -2,6 +2,7 @@ package Programs;
 
 import java.util.stream.Stream;
 
+// FibonacciSeries PROGRAM USING JAVA 8
 public class FibonacciSeries {
     public static void main(String[] args) {
         Stream.iterate(new int[] {0, 1}, f -> new int[] {f[1], f[0]+f[1]}).limit(10).map(f -> f[0]).forEach(i -> System.out.print(i+ " "));
