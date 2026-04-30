10a8903d83a6ce07774c3e2b041bccb43ea6fbde
Sumit Kumar Singh <34736482+amiablemusafir@users.noreply.github.com>
2026-04-30 22:20:26 +0530

    Finding Last Element of Array Using Java 8

--- DSACode/src/main/java/Programs/LastElementOfAnArray.java
+++ DSACode/src/main/java/Programs/LastElementOfAnArray.java
@@ -3,6 +3,7 @@ package Programs;
 import java.util.Arrays;
 import java.util.List;
 
+// Finding Last Element of Array Using Java 8
 public class LastElementOfAnArray {
     public static void main(String[] args) {
         List<String> listOfString = Arrays.asList("Apple", "Ball", "Cat", "Dog", "Ent", "Fan", "Gun");
