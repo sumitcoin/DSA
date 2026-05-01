b79a29cf84db4d8af7b6653ac29763793263cee7
Sumit Kumar Singh <34736482+amiablemusafir@users.noreply.github.com>
2026-05-01 22:33:32 +0530

    Add Two Array And Remove Duplicate Using Java 8

---
+++ DSACode/src/main/java/Programs/AddTwoArrayAndRemoveDuplicate.java
@@ -0,0 +1,18 @@
+package Programs;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class AddTwoArrayAndRemoveDuplicate {
+    public static void main(String[] args) {
+        List<Integer> arr1 = Arrays.asList(3,5,6,7,8,3,9);
+        List<Integer> arr2 = Arrays.asList(6,7,9,3,4,6,7);
+
+        List<Integer> arr3 = Stream.concat(arr1.stream(), arr2.stream()).distinct().collect(Collectors.toList());
+
+        System.out.println(arr3);
+    }
+}
