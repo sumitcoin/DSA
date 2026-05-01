40af98da80b743d68c03443edeee44889bd43398
Sumit Kumar Singh <34736482+amiablemusafir@users.noreply.github.com>
2026-05-01 22:37:37 +0530

    Sum Of First Ten Natural Number Using Java 8

--- DSACode/src/main/java/Programs/SumOfFirstTenNaturalNumber.java
+++ DSACode/src/main/java/Programs/SumOfFirstTenNaturalNumber.java
@@ -2,6 +2,7 @@ package Programs;
 
 import java.util.stream.IntStream;
 
+// Sum of First Ten Natural Number
 public class SumOfFirstTenNaturalNumber {
     public static void main(String[] args) {
         int a = IntStream.range(1, 11).sum();
