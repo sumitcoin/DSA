2985ef8612b939a73bd712ea75fd2d2c7ca2f81f
Sumit Kumar Singh <34736482+amiablemusafir@users.noreply.github.com>
2026-05-02 22:34:32 +0530

    Max number from list

--- DSACode/src/main/java/Programs/GetMaxFromList.java
+++ DSACode/src/main/java/Programs/GetMaxFromList.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
+// Get Max Number from given list
 public class GetMaxFromList {
     public static void main(String[] args) {
         List<Integer> intList = Arrays.asList(33,5,77,89,32,45,98,47);
