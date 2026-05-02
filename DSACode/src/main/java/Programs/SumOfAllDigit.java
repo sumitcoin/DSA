ada2135eb43ab47a8752d8b98a4b19d4bc9474bb
Sumit Kumar Singh <34736482+amiablemusafir@users.noreply.github.com>
2026-05-02 22:40:51 +0530

    Sum of all digit

---
+++ DSACode/src/main/java/Programs/SumOfAllDigit.java
@@ -0,0 +1,14 @@
+package Programs;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class SumOfAllDigit {
+    public static void main(String[] args) {
+        Integer num = 12345;
+
+        Integer sum = Stream.of(String.valueOf(num).split("")).collect(Collectors.summingInt(Integer::parseInt));
+
+        System.out.println(sum);
+    }
+}
